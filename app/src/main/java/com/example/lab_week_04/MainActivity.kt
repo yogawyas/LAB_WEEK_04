43b9b7f3035d95eeb77a13bede41f71da1168628
Yoga Wyas <49479642+yogawyas@users.noreply.github.com>
2025-09-18 13:30:57 +0700

    Commit No. 2 – Bottom Navigation

--- app/src/main/java/com/example/lab_week_04/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_04/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,26 +19,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // set toolbar
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        // Ambil NavHostFragment dari content_main.xml
+        // navHostFragment
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
-        // Set destinasi utama (top level destinations)
+        // top level destination
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.listFragment,       // Coffee List
-                R.id.favoritesFragment   // My Favorite Coffee
+                R.id.listFragment,
+                R.id.favoritesFragment,
+                R.id.cafeFragment
             ),
             findViewById(R.id.drawer_layout)
         )
 
-        // Hubungkan Toolbar + Drawer dengan NavController
+        // action bar
         setupActionBarWithNavController(navController, appBarConfiguration)
-        findViewById<NavigationView>(R.id.nav_view)
-            ?.setupWithNavController(navController)
+
+        // navigation drawer
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        navView.setupWithNavController(navController)
+
+        // bottom navigation
+        val bottomNav: BottomNavigationView = findViewById(R.id.bottom_nav)
+        bottomNav.setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
